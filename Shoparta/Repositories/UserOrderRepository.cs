8ae03d2053c0436f794dcec38f793a00137825f1
Lewapek03 <pawartb@wp.pl>
2024-01-13 22:43:13 +0100

    add view all orders for admin

--- Shoparta/Repositories/UserOrderRepository.cs
+++ Shoparta/Repositories/UserOrderRepository.cs
@@ -10,34 +10,51 @@ namespace Shoparta.Repositories
         private readonly ApplicationDbContext _db;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly UserManager<IdentityUser> _userManager;
+
         public UserOrderRepository(ApplicationDbContext db,
             UserManager<IdentityUser> userManager,
-             IHttpContextAccessor httpContextAccessor)
+            IHttpContextAccessor httpContextAccessor)
         {
             _db = db;
             _httpContextAccessor = httpContextAccessor;
             _userManager = userManager;
         }
+
         public async Task<IEnumerable<Order>> UserOrders()
         {
-            var userId = GetUserId();
-            if (string.IsNullOrEmpty(userId))
-                throw new Exception("User is not logged-in");
-            var orders = await _db.Orders
-                         .Include(x => x.OrderStatus)
+            var user = GetCurrentUser();
+
+            // Sprawdzamy, czy użytkownik to administrator
+            if (await _userManager.IsInRoleAsync(user, "Admin"))
+            {
+                // Jeżeli tak, zwracamy wszystkie zamówienia
+                return await _db.Orders
+                            .Include(x => x.OrderStatus)
                             .Include(x => x.OrderDetail)
-                            .ThenInclude(x => x.Item)
-                            .ThenInclude(x => x.Category)
-                            .Where(a => a.UserId == userId)
+                                .ThenInclude(x => x.Item)
+                                .ThenInclude(x => x.Category)
                             .ToListAsync();
-            return orders;
+            }
+            else
+            {
+                // Jeżeli nie, zwracamy tylko zamówienia dla zalogowanego użytkownika
+                return await _db.Orders
+                            .Include(x => x.OrderStatus)
+                            .Include(x => x.OrderDetail)
+                                .ThenInclude(x => x.Item)
+                                .ThenInclude(x => x.Category)
+                            .Where(a => a.UserId == user.Id)
+                            .ToListAsync();
+            }
         }
 
-        private string GetUserId()
+        private IdentityUser GetCurrentUser()
         {
             var principal = _httpContextAccessor.HttpContext.User;
-            string userId = _userManager.GetUserId(principal);
-            return userId;
+            var userId = _userManager.GetUserId(principal);
+            if (string.IsNullOrEmpty(userId))
+                throw new Exception("User is not logged-in");
+            return _userManager.FindByIdAsync(userId).Result;
         }
     }
 }
